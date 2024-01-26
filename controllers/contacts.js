b0ccfe996a3b9bda0b3d6568e0f4df3fddcc6022
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-01-26 15:41:15 -0700

    added comments

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,25 +1,39 @@
+// Import the 'mongodb' module for database connection
 const mongodb = require('../db/connect');
+
+// Import the 'ObjectId' class from the 'mongodb' library
 const ObjectId = require('mongodb').ObjectId;
 
+// Function to get all contacts from the database
 const getAll = async (req, res, next) => {
+  // Use the 'find' method to get all documents from the 'contacts' collection
   const result = await mongodb.getDb().db().collection('contacts').find();
+
+  // Convert the result to an array and send it as a JSON response
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
 
+// Function to get a single contact by ID from the database
 const getSingle = async (req, res, next) => {
+  // Create an ObjectId from the request parameter 'id'
   const userId = new ObjectId(req.params.id);
+
+  // Use the 'find' method to get a document with the specified ID from the 'contacts' collection
   const result = await mongodb
     .getDb()
     .db()
     .collection('contacts')
     .find({ _id: userId });
+
+  // Convert the result to an array and send the first element as a JSON response
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
 };
 
+// Export the functions to be used in other modules
 module.exports = { getAll, getSingle };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,15 @@
+// Import the 'express' module to create a router
 const express = require('express');
 const router = express.Router();
 
+// Import the 'contactsController' module, which contains functions for handling contact-related routes
 const contactsController = require('../controllers/contacts');
 
+// Define a route to get all contacts
 router.get('/', contactsController.getAll);
 
+// Define a route to get a single contact by ID
 router.get('/:id', contactsController.getSingle);
 
+// Export the router to be used in other modules
 module.exports = router;
