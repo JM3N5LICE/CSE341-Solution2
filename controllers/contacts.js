16438943b25ac75848e86b9bcf90b27adef743e2
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-01-26 20:44:08 -0700

    Modified With PUT, POST, and DELETE

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -35,5 +35,72 @@ const getSingle = async (req, res, next) => {
   });
 };
 
+const createContact = async (req, res, next) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({
+        success: true,
+        contact: response.ops[0],
+        message: 'Contact created successfully.'
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        error: response.error || 'Some error occurred while creating the contact.'
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: 'Internal Server Error'
+    });
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
 // Export the functions to be used in other modules
-module.exports = { getAll, getSingle };
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -11,5 +11,12 @@ router.get('/', contactsController.getAll);
 // Define a route to get a single contact by ID
 router.get('/:id', contactsController.getSingle);
 
+// Define a route to post a contact
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
 // Export the router to be used in other modules
 module.exports = router;
