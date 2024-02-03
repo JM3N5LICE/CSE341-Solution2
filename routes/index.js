e434fe9ad06e94bf4813b8f9c53d7cff2f12198e
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-02-02 20:49:48 -0700

    swagger connection

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Use the '/contacts' path and include the routes defined in the 'contacts' module
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
 
 // Export the router to be used in other modules
