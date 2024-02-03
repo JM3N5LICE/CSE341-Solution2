fcfed3aad703310a9c00c50e4e0195533ed0fa78
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-02-02 21:10:22 -0700

    https

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: "Names",
     },
     host: "test-solution2.onrender.com",
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
